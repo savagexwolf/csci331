954763ec81455708498147831d2e852509c5ceab
savagexwolf <113149587+savagexwolf@users.noreply.github.com>
2025-10-26 23:14:01 -0600

    Delete mobile_dev-starter directory

--- mobile_dev-starter/scripts/app.js
+++
@@ -1,5 +0,0 @@
-// set footer year
-const yearSpan = document.getElementById("year").textContent = new Date().getFullYear();
-if (yearSpan) {
-    yearSpan.textContent = new Date().getFullYear();
-}
